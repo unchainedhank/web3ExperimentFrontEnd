a83f83ea744a019d405ced86792c8049cbb48cf6
unchainedhank <2450312896@qq.com>
2020-10-20 11:16:46 +0800

    basic login and success function built.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,29 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import axios from 'axios'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+// import './element'
+// import VueAxios from 'vue-axios'
 
-Vue.config.productionTip = false
+
+Vue.config.productionTip = false;
+Vue.prototype.$axios = axios;
+Vue.use(VueRouter);
+Vue.use(ElementUI);
+
+
+
+const routes = new VueRouter({
+  routes: [
+    { path: '/', component: () => import('../src/pages/Login.vue')},
+    { path: '/success', component: () => import('../src/pages/Success')}
+  ]
+})
+
 
 new Vue({
   render: h => h(App),
+  router: routes
 }).$mount('#app')
